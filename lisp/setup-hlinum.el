3389c8ef47b51f35a1791b09a76735014fe9c84f
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2015-07-11 23:17:45 +0200

    changes on mac

---
+++ lisp/setup-hlinum.el
@@ -0,0 +1,29 @@
+(require 'hlinum)
+;; (hlinum-activate)
+(defface my-linum-hl
+  `((t :inherit linum :background ,(face-background 'hl-line nil t)))
+  "Face for the current line number."
+  :group 'linum)
+
+(defvar my-linum-format-string "%3d")
+
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+
+(defun my-linum-get-format-string ()
+  (let* ((width (1+ (length (number-to-string
+                             (count-lines (point-min) (point-max))))))
+         (format (concat "%" (number-to-string width) "d")))
+    (setq my-linum-format-string format)))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-format)
+
+(defun my-linum-format (line-number)
+  (propertize (format my-linum-format-string line-number) 'face
+              (if (eq line-number my-linum-current-line-number)
+                  'my-linum-hl
+                'linum)))
+
+(provide 'setup-hlinum)
+;;; setup-hlinum.el ends here
---
+++ lisp/setup-powerline.el
@@ -0,0 +1,9 @@
+(require 'powerline)
+(powerline-default-theme)
+
+(setq powerline-arrow-shape 'curve)
+
+;; (set-face-attribute 'mode-line nil
+;;                     :foreground "Black"
+;;                     :background "DarkOrange"
+;;                     :box nil)
