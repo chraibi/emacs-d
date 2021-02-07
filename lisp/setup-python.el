c81e3a291acbcb8794f21afc6c020d30c9f93068
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2021-02-07 17:33:42 +0100

    some pyhon confis

--- lisp/setup-python.el
+++ lisp/setup-python.el
@@ -2,26 +2,27 @@
 ;;; Commentary:
 
 (require 'python)
+(elpy-enable)
+;(add-hook 'python-mode-hook 'fci-mode)
 ;(require 'setup-hlinum)
 (message "Activate python mode")
-(package-initialize)
+;(package-initialize)
 ;;; Code:
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (setq ipython-command "/usr/local/bin/ipython")
 (setq python-shell-interpreter "/usr/local/bin/python3")
 (setq py-python-command "/usr/local/bin/python3")
 (setq flycheck-python-pycompile-executable "python3")
-(setq indetn-tabs-mode nil)
+(setq indent-tabs-mode nil)
 (setq python-indent-guess-indent-offset 1)
 (linum-mode 1)
-(elpy-mode 1)
+
 (auto-complete-mode 1)
-(setq python-indent-offset 4)
+;; (python-indent-offset . 4)
 
+;; (require 'fill-column-indicator)
 
-(require 'fill-column-indicator)
-(add-hook 'python-mode-hook 'fci-mode)
-(setq fci-rule-color "black")                        
+;; (setq fci-rule-color "black")
 
 (defun flycheck-python-setup ()
   (flycheck-mode)
@@ -32,7 +33,11 @@
 ;; ;; Automatically remove trailing whitespace when file is saved.
 
 
-(setq py-python-command-args '( "--colors=Linux"))
+(add-hook 'python-mode-hook (lambda ()
+                              (require 'sphinx-doc)
+                              (sphinx-doc-mode t)))
+
+;; (setq py-python-command-args '( "--colors=Linux"))
 (message "leave python setup")
 (provide 'setup-python)
 ;;; setup-python.el ends here
