9353c908188de6af2411c7b615db124d551532ae
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2021-02-07 17:34:22 +0100

    snippets for python

--- snippets/python-mode/.yas-setup.el
+++ snippets/python-mode/.yas-setup.el
@@ -1,19 +1,19 @@
-(defun python-split-args (arg-string)
-  "Split a python argument string into ((name, default)..) tuples"
-  (mapcar '(lambda (x)
-             (split-string x "[[:blank:]]*=[[:blank:]]*" t))
-          (split-string arg-string "[[:blank:]]*,[[:blank:]]*" t)))
-
-(defun python-args-to-docstring ()
-  "return docstring format for the python arguments in yas-text"
+(defun python-args-to-google-docstring (text &optional make-fields)
+  "Return a reST docstring format for the python arguments in yas-text."
   (let* ((indent (concat "\n" (make-string (current-column) 32)))
-         (args (python-split-args yas-text))
-         (max-len (if args (apply 'max (mapcar '(lambda (x) (length (nth 0 x))) args)) 0))
-         (formatted-args (mapconcat
-                '(lambda (x)
-                   (concat (nth 0 x) (make-string (- max-len (length (nth 0 x))) ? ) " -- "
-                           (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
-                args
-                indent)))
+         (args (python-split-args text))
+     (nr 0)
+         (formatted-args
+      (mapconcat
+       (lambda (x)
+         (concat "   " (nth 0 x)
+             (if make-fields (format " ${%d:arg%d}" (cl-incf nr) nr))
+             (if (nth 1 x) (concat " \(default " (nth 1 x) "\)"))))
+       args
+       indent)))
     (unless (string= formatted-args "")
-      (mapconcat 'identity (list "Keyword Arguments:" formatted-args) indent))))
+      (concat
+       (mapconcat 'identity
+          (list "" "Args:" formatted-args)
+          indent)
+       "\n"))))
