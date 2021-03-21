092e054642f4d6b1632e0adbc6c51883c35b22cd
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2021-03-21 22:22:45 +0100

    remove ivy

--- lisp/setup-ivy.el
+++ lisp/setup-ivy.el
@@ -3,10 +3,10 @@
 ;;; Commentary:
 
 (message "Enter setup-ivy")
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
+;(setq ivy-use-virtual-buffers t)
+;(setq enable-recursive-minibuffers t)
+;(global-set-key "\C-s" 'swiper)
+;(global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
